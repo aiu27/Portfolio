55a2fe15cc64599799d9ffc4c302f2137e6d06e6
aiu27 <88765710+aiu27@users.noreply.github.com>
2022-02-13 17:11:50 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const imgs = document.querySelector(".gallery");
+console.log(imgs);
+// Ececute when imgs is clocked
+imgs.addEventListener("click", function (e) {
+  // Get the expanded image
+  const expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  const imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = e.target.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = e.target.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+
+  // Align footer at bottom
+  document.getElementById("m_page2").style.padding = "0 0 50px 0";
+});
+
+// Ececute when closebtn is clicked
+document.querySelector(".closebtn").addEventListener("click", function () {
+  // Hide the expanded image (shown by above eventListener)
+  document.querySelector(".closebtn").parentElement.style.display = "none";
+});
